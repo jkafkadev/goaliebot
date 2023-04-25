ed194828b5225e7229375d79862d83542df5d327
kafka050 <jkafkadev@gmail.com>
2023-04-25 14:11:02 -0500

    Finds position of ball each callback

--- scripts/move_turtlebot.py
+++ scripts/move_turtlebot.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import Twist
+from tf import transformations
+from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import LaserScan
+ball_position = [0, 0, 0]
 
-def laserscan_callback():
-    print("boop")
+def find_ball_position(distance, angle):
+    return [distance*transformations.math.cos(angle), distance*transformations.math.sin(angle)]
+
+
+def laserscan_callback(msg):
+    ball_hits = []
+    ball_angles = []
+    for i in range(len(msg.ranges)):
+        if msg.ranges[i] < 10:
+            ball_hits.append(msg.ranges[i])
+            ball_angles.append(i)
+    ball_distance = ball_hits[transformations.math.floor(len(ball_hits)/2)]
+    ball_angle = ball_angles[transformations.math.floor(len(ball_angles)/2)]*transformations.math.pi/180
+    print(find_ball_position(ball_distance, ball_angle))
+    
+    
+
 
 
 def control_loop(direction):
@@ -21,17 +38,17 @@ def control_loop(direction):
 
 rospy.init_node('topic_publisher')
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-sub = rospy.Subscriber('/scan', LaserScan, laserscan_callback())
+sub = rospy.Subscriber('/scan', LaserScan, laserscan_callback)
 rate = rospy.Rate(2)
-move = Twist()
+#move = Twist()
 
 ball_direction = "left"
 
 
 ##########  MAIN LOOP ##########
 while not rospy.is_shutdown():
-    pub.publish(move)
+    #pub.publish(move)
     rate.sleep()
 
-    control_loop(ball_direction)
+    #control_loop(ball_direction)
     
